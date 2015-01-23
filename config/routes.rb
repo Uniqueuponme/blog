44d8511d38a9edca7943348e3b405f7f67d071cb
AJ PERKINS <ajperk@gmail.com>
2015-01-22 22:11:00 -0500

    Before Authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'welcome/index'
-  resources :articles
+  resources :articles do
+    resources :comments
+end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -55,4 +57,5 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
 end
